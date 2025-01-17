7fb25c630dfd18d09b6b05a6e3d0584aa8a87392
Anthony Schwab <schwab.it@pm.me>
2025-01-16 18:43:33 -0600

    add helper.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
+import writeToFile from '/helper.js'
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'Enter the project title:',
+    'Enter the project description:',
+    'Enter the installation instructions:',
+    'Enter the usage information:',
+    'Enter contribution guidelines:',
+    'Enter test instructions:'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
