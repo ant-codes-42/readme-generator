176499155682bf8d71f0491a0d0ee256d22f2272
Anthony Schwab <schwab.it@pm.me>
2025-01-16 20:07:50 -0600

    completed questions array

--- index.js
+++ index.js
@@ -3,19 +3,70 @@ import writeToFile from '/helper.js'
 
 // TODO: Create an array of questions for user input
 const questions = [
-    'Enter the project title:',
-    'Enter the project description:',
-    'Enter the installation instructions:',
-    'Enter the usage information:',
-    'Enter contribution guidelines:',
-    'Enter test instructions:'
+    {
+        message: 'Enter the project title:',
+        type: 'input',
+        name: 'title'
+    },
+    {
+        message: 'Enter the project description:',
+        type: 'editor',
+        name: 'description'
+    },
+    {
+        message: 'Enter the installation instructions:',
+        type: 'editor',
+        name: 'install'
+    },
+    {
+        message: 'Enter the usage information:',
+        type: 'editor',
+        name: 'usage'
+    },
+    {
+        message: 'Enter contribution guidelines:',
+        type: 'editor',
+        name: 'contribution'
+    },
+    {
+        message: 'Enter test instructions:',
+        type: 'editor',
+        name: 'test'
+    },
+    {
+        message: 'Choose a license from the list:',
+        type: 'list',
+        choices: [
+            { name: 'Apache License 2.0', value: 'apache' },
+            { name: 'GNU General Public License v3.0', value: 'gnuGPU3' },
+            { name: 'MIT License', value: 'mit' },
+            { name: 'BSD 2-Clause "Simplified" License', value: 'BSD2' },
+            { name: 'BSD 3-Clause "New" or "Revised" License', value: 'BSD3' },
+            { name: 'Boost Software License 1.0', value: 'boost' },
+            { name: 'Creative Commons Zero v1.0 Universal', value: 'ccz' }
+        ],
+        name: 'license'
+    },
+    {
+        message: 'Enter your GitHub username:',
+        type: 'input',
+        name: 'githubUsername'
+    },
+    {
+        message: 'Enter your email address:',
+        type: 'input',
+        name: 'emailAdress'
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// Loop through the questions array using the inquirerPrompt function
+function init() {
+
+}
 
 // Function call to initialize app
 init();
